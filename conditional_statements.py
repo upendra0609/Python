01cb027e184549838398c4c043ca3f1932b5e7c1
Upendra <upendrasatna@gmail.com>
2023-10-07 14:34:34 +0530

    round function

---
+++ conditional_statements.py
@@ -0,0 +1,12 @@
+age = int(input("enter your age: "))
+if age >= 18:
+    print("eligible for voting")
+else:
+    print("not eligible for voting")
+
+# even-odd number
+num = int(input("Enter number"))
+if num % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
---
+++ exercise.py
@@ -0,0 +1,4 @@
+age = float(input("enter your age"))
+remainingYear = round(age)
+remainingAge = 90 - age;
+
