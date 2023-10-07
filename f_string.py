797dbb124427d045a7723dd073e00bd19262a2d9
Upendra <upendrasatna@gmail.com>
2023-10-07 12:49:35 +0530

    round function

---
+++ f_string.py
@@ -0,0 +1,4 @@
+name ="krishna"
+age = 24
+height = 1.5
+print(f"I am {name}. I am {age} year old. my height is {height} meter")
--- round_function.py
+++ round_function.py
@@ -1,12 +1,16 @@
 # in case of xx.5 return nearest even integer
-a = round(12.5, 0)
+a = round(12.5)
 b = round(11.5, 0)
 print(a, b)
 
 # if we pass -ve number then it will affect integer also
-# 10^(-no of digits)  i.e 10^(-(-1)) = 10 closest to 10 multiple number
+# 10^(-no of digits)  i.e 10^(-(-1)) = 10 closest to multiple of 10
 a = round(123, -1)
 print(a)
 a = round(12.67, -1)
 print(a)
 
+a = round(553, -3)
+print(a)
+a = round(453, -3)
+print(a)
